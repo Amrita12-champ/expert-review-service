636bebe41e1163ae0fe59118b7ff2f41f6cb5963
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-09-24 23:03:33 +0530

    Initial comit to exert-review-microservice of product development

---
+++ src/main/java/com/example/expertreviewservice/ExpertReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.expertreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertReviewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertReviewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expertreviewservice/ExpertReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expertreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
