b9bbade166b20d6a3073450c598bfb4840ccc58a
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-09-26 21:35:49 +0530

    added the reviewservice business logic of project

--- src/main/java/com/example/expertreviewservice/repository/ExpertReviewRepository.java
+++ src/main/java/com/example/expertreviewservice/repository/ExpertReviewRepository.java
@@ -1,4 +1,14 @@
 package com.example.expertreviewservice.repository;
 
-public interface ExpertReviewRepository {
+import com.example.expertreviewservice.entity.ExpertReview;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ExpertReviewRepository extends JpaRepository<ExpertReview, String> {
+    List<ExpertReview> findByExpertId(String expertId);
+    List<ExpertReview> findbyStatus(String status);
+    List<ExpertReview> findByDiagnosisId(String diagnosisId);
+
+
 }
--- src/main/java/com/example/expertreviewservice/service/ExpertReviewService.java
+++ src/main/java/com/example/expertreviewservice/service/ExpertReviewService.java
@@ -1,4 +1,15 @@
 package com.example.expertreviewservice.service;
 
+import com.example.expertreviewservice.entity.ExpertReview;
+import com.example.expertreviewservice.repository.ExpertReviewRepository;
+
+import java.util.List;
+
 public interface ExpertReviewService {
+
+    ExpertReview createReview(ExpertReview e1);
+    ExpertReview getReviewById(String reviewId);
+    List<ExpertReview> getAllReviews();
+    List<ExpertReview>getReviewByStatus(String status);
+    ExpertReviewRepository updateReviewStatus(String reviwId, String status, String description);
 }
